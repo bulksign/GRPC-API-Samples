b04cec0dfb415947bdc98aa890cf7e197ee92222
Bulksign <contact@bulksign.com>
2023-07-06 17:00:45 +0300

    added samples for the new APIs introduced in version 4.64

---
+++ Scenarios/DownloadEnvelopeAttachments.cs
@@ -0,0 +1,48 @@
+﻿using Bulksign.Api;
+using GrpcApiSamples;
+
+namespace Bulksign.ApiSamples;
+
+public class DownloadEnvelopeAttachments
+{
+	public void RunSample()
+	{
+		const string ENVELOPE_ID = "your_envelope_id";
+
+
+		AuthenticationApiModel token = new Authentication().GetAuthenticationModel();
+
+		if (string.IsNullOrEmpty(token.Key))
+		{
+			Console.WriteLine("Please edit Authentication.cs and set your own API key there");
+			return;
+		}
+
+		try
+		{
+
+			EnvelopeIdInput ei = new EnvelopeIdInput()
+			{
+				Authentication = token,
+				EnvelopeId     = ENVELOPE_ID
+			};
+
+			DownloadEnvelopeCompletedAttachmentsResult  result = ChannelManager.GetClient().DownloadEnvelopeCompletedAttachments(ei);
+
+			if (result.IsSuccessful)
+			{
+				//the result here will by a byte[] of a zip file which contains ALL PDF attachments from all the envelope documents
+				Console.WriteLine($"File size :  {result.Result.Length}");
+			}
+			else
+			{
+				Console.WriteLine("ERROR : " + result.ErrorCode + " " + result.ErrorMessage);
+			}
+		}
+		catch (Exception bex)
+		{
+			//handle failed request here. See
+			Console.WriteLine($"Exception {bex.Message}");
+		}
+	}
+}
---
+++ Scenarios/ResetSignatureType.cs
@@ -0,0 +1,54 @@
+﻿using Bulksign.Api;
+using GrpcApiSamples;
+
+namespace Bulksign.ApiSamples;
+
+public class ResetSignatureType
+{
+
+	public void RunSample()
+	{
+		AuthenticationApiModel token = new Authentication().GetAuthenticationModel();
+
+		if (string.IsNullOrEmpty(token.Key))
+		{
+			Console.WriteLine("Please edit Authentication.cs and set your own API key there");
+			return;
+		}
+
+		try
+		{
+			ResetSignatureTypeApiModelInput model = new ResetSignatureTypeApiModelInput()
+			{
+				Authentication = token,
+
+				EnvelopeId = "your_envelope_id",
+
+				//the new type of signature which will be assigned to the recipient
+				SignatureType = Api.ResetSignatureType.ClickToSign,
+
+				//we'll specify the recipient by the index (in this sample being the first recipient)
+				ByIndex = new FindRecipientByIndexApiModel()
+				{
+					Index = 1
+				}
+			};
+
+			NumericResult result = ChannelManager.GetClient().ResetSignatureType(model);
+
+			if (result.IsSuccessful)
+			{
+				Console.WriteLine($"{result.Result} signature fields were changed");
+			}
+			else
+			{
+				Console.WriteLine("ERROR : " + result.ErrorCode + " " + result.ErrorMessage);
+			}
+		}
+		catch (Exception bex)
+		{
+			//handle failed request here
+			Console.WriteLine($"Exception {bex.Message}");
+		}
+	}
+}
